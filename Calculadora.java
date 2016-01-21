69d0f08c79fe48fe86b4d8dd6a96669cbc0a1815
paco <franmenendezpouso@gmail.com>
2016-01-21 11:02:49 +0100

    metodo q devuelve por pantalla multiplos de 5

---
+++ Calculadora.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Calculadora here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Calculadora
+{
+    // instance variables - replace the example below with your own
+    
+    
+    
+    public Calculadora(){
+       
+        
+    }
+    
+    public void multiplesOfFive(){
+        int numero = 5;
+        int multi = 0;
+        while((numero*multi)<95){
+            
+            
+            if((numero*multi)>10){
+                
+                
+                System.out.println(numero+" x "+multi+" = "+(numero*multi));
+            }
+        
+        multi++;
+        }
+    
+    }
+}
